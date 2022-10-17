201fa1f033ca463042176e1f686d657b302155fb
biplab5464 <bmg321111@outlook.com>
2022-10-17 15:05:55 +0530

    nohting just changes to closure and added gitignore file

--- closure.rs
+++ closure.rs
@@ -1,4 +1,13 @@
+use std::thread;
+
 fn main(){
+    
+    part1();
+    part2();
+    part3();
+}
+
+fn part1(){
     let list = vec![1,2,3];
     println!("Before definign closure: {:?}",list);
 
@@ -7,5 +16,23 @@ fn main(){
     println!("Before calling closure: {:?}",list);
     only_borrows();
     println!("After calling closure: {:?}",list);
+}
 
+fn part2(){
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
+}
+
+fn part3(){
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
 }
