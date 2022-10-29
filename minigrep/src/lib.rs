cd01264fc62035ce2a863714eba60d005f2311c7
biplab5464 <bmg321111@outlook.com>
2022-10-29 16:14:03 +0530

    changes to search and search_case_insensitive bfuction (introduced the iterater over loop)

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -48,26 +48,21 @@ pub fn run( config: Config) -> Result<(),Box<dyn std::error::Error>>{
     Ok(())
 }
 
-pub fn search<'a>(query : &str, content : &'a str) ->  Vec<&'a str>{
-    let mut results = Vec::new();
-    for line in content.lines(){
-        if line.contains(query){
-            results.push(line);
-        }
-    }
-    results
+pub fn search<'a>(query : &str,contents : &'a str) -> Vec<&'a str>{
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
-pub fn search_case_insensitive<'a>( query : &str, contents : &'a str) -> Vec<&'a str>{
+pub fn search_case_insensitive<'a>(query : &str, contents : &'a str) -> Vec<&'a str>{
     let query = query.to_lowercase();
-    let mut results = Vec::new();
+    
+    contents
+        .lines()
+        .filter(|line | line.to_lowercase().contains(&query))
+        .collect()
 
-    for line in contents.lines(){
-        if line.to_lowercase().contains(&query){
-            results.push(line)
-        }
-    }
-    results
 }
 
 #[cfg(test)]
