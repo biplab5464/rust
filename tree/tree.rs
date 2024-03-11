53b792750da05a9777b1500a9bce56e312af0848
biplab5464 <biplab5464@outlook.com>
2024-03-12 00:01:31 +0530

    tree object

---
+++ tree/tree.rs
@@ -0,0 +1,48 @@
+#[derive(Debug)]
+struct TreeNode{
+    data : u32,
+    left : Node,
+    right : Node
+}
+
+type Node = Option<Box<TreeNode>>;
+
+#[derive(Debug)]
+struct Tree{
+    root : TreeNode
+}
+
+fn main(){
+
+    let mut tree = Tree{
+        root : TreeNode{
+            data : 45,
+            left : None,
+            right : Some(Box::new(TreeNode{
+                data : 23,
+                left : None,
+                right : Some(Box::new(TreeNode{
+                    data : 36,
+                    left : None,
+                    right : None
+                }))
+            }))
+        }
+    };
+
+    tree.root.left = Some(Box::new(TreeNode{
+        data : 72,
+        left : Some(Box::new(TreeNode{
+            data : 89,
+            left : None,
+            right : None
+        })),
+        right : Some(Box::new(TreeNode{
+            data : 43,
+            left : None,
+            right : None
+        }))
+    }));
+
+    println!("Tree - {:?}",tree);
+}
