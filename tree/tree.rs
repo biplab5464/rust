8adb29d2816c4042c324b423bc1a50c53b9186e5
biplab5464 <biplab5464@outlook.com>
2024-03-12 22:45:22 +0530

    traversal added

--- tree/tree.rs
+++ tree/tree.rs
@@ -9,13 +9,13 @@ type Node = Option<Box<TreeNode>>;
 
 #[derive(Debug)]
 struct Tree{
-    root : TreeNode
+    root : Node
 }
 
 fn main(){
 
     let mut tree = Tree{
-        root : TreeNode{
+        root : Some(Box::new(TreeNode{
             data : 45,
             left : None,
             right : Some(Box::new(TreeNode{
@@ -27,10 +27,10 @@ fn main(){
                     right : None
                 }))
             }))
-        }
+        }))
     };
 
-    tree.root.left = Some(Box::new(TreeNode{
+    tree.root.as_mut().unwrap().left = Some(Box::new(TreeNode{
         data : 72,
         left : Some(Box::new(TreeNode{
             data : 89,
@@ -44,7 +44,7 @@ fn main(){
         }))
     }));
 
-    let farther_node = &mut tree.root.left.as_mut().unwrap();
+    let farther_node = &mut tree.root.as_mut().unwrap().left.as_mut().unwrap();
 
     //println!("Tree - {:?}",farther_node.left);
     farther_node.left = Some(Box::new(TreeNode{
@@ -55,4 +55,34 @@ fn main(){
     }));
     println!("Tree - {:?}",tree);
 
+
+    // traversal of tree
+    print!("Tree -> ");
+    inOrder(&tree.root);
+    println!()
+
+}
+
+fn inOrder(tree : &Node){
+    if let Some(node) = tree{
+        inOrder(&node.left);
+        print!("{},",node.data);
+        inOrder(&node.right);
+    }
+}
+
+fn preOrder(tree : &Node){
+    if let Some(node) = tree{
+        print!("{},",node.data);
+        inOrder(&node.left);
+        inOrder(&node.right);
+    }
+}
+
+fn postOrder(tree : &Node){
+    if let Some(node) = tree{
+        inOrder(&node.left);
+        inOrder(&node.right);
+        print!("{},",node.data);
+    }
 }
