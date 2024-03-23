446cc4dc7530443fdc264651ce89a266620da2a4
biplab5464 <bmg321111@outlook.com>
2024-03-23 12:29:00 +0530

    moved all the things to impl (code looks good now)

--- tree/tree.rs
+++ tree/tree.rs
@@ -15,149 +15,173 @@ struct Tree{
     root : Node
 }
 
-fn main(){
+impl Tree{
 
-    let mut tree = Tree{
-        root : Some(Box::new(TreeNode{
-            data : 53,
+    fn create_root(data : u32)-> Tree{
+        Tree{
+            root : Some(Box::new(TreeNode{
+            data,
             left : None,
             right : None
-        }))
-    };
-    insert(&mut tree.root,32);
-    insert(&mut tree.root,78);
-    insert(&mut tree.root,45);
-    insert(&mut tree.root,47);
-    insert(&mut tree.root,63);
+        }))}
+    }
+
+    fn in_order(&self){
+        print!("Tree Inorder-> ");
+        Self::in_order_taversal(&self.root);
+        println!();
+    }
+    
+    fn pre_order(&self){
+        print!("Tree Preorder -> ");
+        Self::pre_order_traversal(&self.root);
+        println!();
+    }
+    
+    fn post_order(&self){
+        print!("Tree Postorder -> ");
+        Self::post_order_traversal(&self.root);
+        println!();
+    }
+    
+    
+    fn in_order_taversal(tree : &Node){
+        if let Some(node) = tree{
+            Self::in_order_taversal(&node.left);
+            print!("{},",node.data);
+            Self::in_order_taversal(&node.right);
+        }
+    }
+    
+    fn pre_order_traversal(tree : &Node){
+        if let Some(node) = tree{
+            print!("{},",node.data);
+            Self::pre_order_traversal(&node.left);
+            Self::pre_order_traversal(&node.right);
+        }
+    }
+    
+    fn post_order_traversal(tree : &Node){
+        if let Some(node) = tree{
+            Self:: post_order_traversal(&node.left);
+            Self::post_order_traversal(&node.right);
+            print!("{},",node.data);
+    }
+    }
+        }
+    }
+    
+    fn insert(&mut self,ele : u32){
+        Self::insert_node(&mut self.root,ele);
+    }
+
+    fn insert_node(tree : &mut Node,ele : u32){
+        let mut my_node = tree.as_mut().unwrap();
+     
+        let new_node = Some(Box::new(TreeNode{
+            data : ele,
+            left : None,
+            right : None
+        }));
+    
+        if my_node.data > ele {
+            if my_node.left.is_none(){
+                my_node.left = new_node;
+            }else{
+                Self::insert_node(&mut my_node.left,ele);
+            }
+        }else{
+            if my_node.right.is_none(){
+                my_node.right = new_node;
+            }else{
+                Self::insert_node(&mut my_node.right,ele);
+            }
+        }
+    }
+    
+    fn search(&self,ele : u32) -> bool{
+        Self::search_node(&self.root,ele)
+    }
+
+    fn search_node(tree : &Node, ele : u32) -> bool{
+        if let Some(node) = tree{
+            return match ele.cmp(&node.data){
+                Ordering::Less => Self::search_node(&node.left,ele),
+                Ordering::Equal => true,
+                Ordering::Greater => Self::search_node(&node.right,ele),
+            }
+        }
+        false
+    }
+    
+    fn display(&self){
+        Self::display_tree(&self.root);
+        print!("\n");
+    }
+    
+    fn display_tree(node: &Node) {
+        if let Some(ref inner) = node {
+            print!("( [{}] ", inner.data); 
+            print!(" left :"); 
+            Self::display_tree(&inner.left);
+            print!(" right :"); 
+            Self::display_tree(&inner.right);
+            print!(" )"); 
+        } else {
+            print!(" None");
+        }
+    }
+    
+    fn total_node(&self) -> usize {
+        let mut count_node : usize = 0;  
+        Self::count(&self.root,&mut count_node);
+        count_node
+    }
+    
+    fn count(tree : &Node, count_node  : &mut usize){
+        if let Some(node) = tree{
+            Self::count(&node.left,count_node);
+            *count_node +=1;
+            Self::count(&node.right,count_node);
+        }
+    }
+    
+    fn total_leaf_node( &self ) -> usize {
+        let mut count_node : usize = 0;  
+        Self::count_leaf(&self.root,&mut count_node);
+        count_node
+    }
+    
+    fn count_leaf(tree : &Node, count_node  : &mut usize){
+        if let Some(node) = tree{
+            Self::count_leaf(&node.left,count_node);
+            if node.left.is_none() && node.right.is_none(){
+                *count_node +=1;
+            }
+            Self::count_leaf(&node.right,count_node);
+        }
+    }
+}
+
+fn main(){
+
+    let mut tree = Tree::create_root(53);
+    tree.insert(32);
+    tree.insert(78);
+    tree.insert(45);
+    tree.insert(47);
+    tree.insert(63);
 
     //println!("Tree - {:?}",tree);
-    display(&tree);
+    tree.display();
     // traversal of tree
-    print!("Tree -> ");
-    inOrder(&tree);
-    println!();
-    println!("did you find the ele {}",search(&tree.root,45));
-    println!("number of node in the {}",total_node(&tree));
-    println!("number of leaf node in the {}",total_leaf_node(&tree));
+    tree.in_order();
+    tree.pre_order();
+    tree.post_order();
+
+    println!("did you find the ele {}",tree.search(45));
+    println!("number of node in the {}",tree.total_node());
+    println!("number of leaf node in the {}",tree.total_leaf_node());
 
 }
 
-fn inOrder(tree : &Tree){
-    inOrder_taversal(&tree.root);
-}
-
-fn preOrder(tree : &Tree){
-    preOrder_traversal(&tree.root);
-}
-
-fn postOrder(tree : &Tree){
-    postOrder_traversal(&tree.root);
-}
-
-
-fn inOrder_taversal(tree : &Node){
-    if let Some(node) = tree{
-        inOrder_taversal(&node.left);
-        print!("{},",node.data);
-        inOrder_taversal(&node.right);
-    }
-}
-
-fn preOrder_traversal(tree : &Node){
-    if let Some(node) = tree{
-        print!("{},",node.data);
-        preOrder_traversal(&node.left);
-        preOrder_traversal(&node.right);
-    }
-}
-
-fn postOrder_traversal(tree : &Node){
-    if let Some(node) = tree{
-        postOrder_traversal(&node.left);
-        postOrder_traversal(&node.right);
-        print!("{},",node.data);
-    }
-}
-
-fn insert(tree : &mut Node,ele : u32){
-    let mut my_node = tree.as_mut().unwrap();
- 
-    let new_node = Some(Box::new(TreeNode{
-        data : ele,
-        left : None,
-        right : None
-    }));
-
-    if my_node.data > ele {
-        if my_node.left.is_none(){
-            my_node.left = new_node;
-        }else{
-            insert(&mut my_node.left,ele);
-        }
-    }else{
-        if my_node.right.is_none(){
-            my_node.right = new_node;
-        }else{
-            insert(&mut my_node.right,ele);
-        }
-    }
-}
-
-fn search(tree : &Node, ele : u32) -> bool{
-    if let Some(node) = tree{
-        return match ele.cmp(&node.data){
-            Ordering::Less => search(&node.left,ele),
-            Ordering::Equal => true,
-            Ordering::Greater => search(&node.right,ele),
-        }
-    }
-    false
-}
-
-fn display(tree : &Tree){
-    display_tree(&tree.root);
-    print!("\n");
-}
-
-fn display_tree(node: &Node) {
-    if let Some(ref inner) = node {
-        print!("( [{}] ", inner.data); 
-        print!(" left :"); 
-        display_tree(&inner.left);
-        print!(" right :"); 
-        display_tree(&inner.right);
-        print!(" )"); 
-    } else {
-        print!(" None");
-    }
-}
-
-fn total_node( tree : &Tree ) -> usize {
-    let mut count_node : usize = 0;  
-    count(&tree.root,&mut count_node);
-    count_node
-}
-
-fn count(tree : &Node, count_node  : &mut usize){
-    if let Some(node) = tree{
-        count(&node.left,count_node);
-        *count_node +=1;
-        count(&node.right,count_node);
-    }
-}
-
-fn total_leaf_node( tree : &Tree ) -> usize {
-    let mut count_node : usize = 0;  
-    count_leaf(&tree.root,&mut count_node);
-    count_node
-}
-
-fn count_leaf(tree : &Node, count_node  : &mut usize){
-    if let Some(node) = tree{
-        count_leaf(&node.left,count_node);
-        if node.left.is_none() && node.right.is_none(){
-            *count_node +=1;
-        }
-        count_leaf(&node.right,count_node);
-    }
-}
